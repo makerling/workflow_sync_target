a12940c4c87ca8f18416cddfa9ac7f2b338bf634
Stevan Vanderwerf <github@vanderling.com>
2021-11-14 21:36:37 -0600

    removed comments

--- sync.py
+++ sync.py
@@ -1,8 +1,6 @@
 from dirsync import sync
 
-# sourcedir='/home/runner/work/workflow_sync_target/workflow_sync_target/workflow_sync_source/files'
 sourcedir='workflow_sync_source/files'
-# targetdir='/home/runner/work/workflow_sync_target/workflow_sync_target/files'
 targetdir='workflow_sync_target/files'
 action='sync'
 
