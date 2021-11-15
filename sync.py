6d919a381f2436bc3e629cfafb306819789d3eac
Stevan Vanderwerf <github@vanderling.com>
2021-11-14 19:22:28 -0600

    debugging file path issues

--- sync.py
+++ sync.py
@@ -1,7 +1,7 @@
 from dirsync import sync
 
-sourcedir='files'
-targetdir='files'
+sourcedir='/home/runner/work/workflow_sync_target/workflow_sync_target/workflow_sync_source/files'
+targetdir='/home/runner/work/workflow_sync_target/workflow_sync_target/files'
 action='sync'
 
 sync(sourcedir, targetdir, action)
